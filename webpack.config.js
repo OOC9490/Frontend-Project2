d8284b38c0e1f80d5faf25e39cd90791e13903bb
Zara <zara@okaayfinee.home>
2020-04-06 22:35:09 +1000

    Added sass to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,29 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
-      }
+      // Apply rule for .sass, .scss or .css files
+      test: /\.(sa|sc|c)ss$/,
+      // Set loaders to transform files.
+      // Loaders are applying from right to left(!)
+      // The first loader will be applied after others
+      use: [
+             {
+               // This loader resolves url() and @imports inside CSS
+               loader: "css-loader",
+             },
+             {
+               // Then we apply postCSS fixes like autoprefixer and minifying
+               loader: "postcss-loader"
+             },
+             {
+               // First we transform SASS to standard CSS
+               loader: "sass-loader",
+               options: {
+                 implementation: require("sass")
+               }
+             }
+           ]
+    }
     ]
   },
   plugins: [
