d0557c18c11fe3d25c76012f454b3f6fa4fe9e16
Zara <zara@okaayfinee.home>
2020-04-06 22:18:20 +1000

    ugly css and react routes - Z

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,11 @@
 //import component from "./components/componentName";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
