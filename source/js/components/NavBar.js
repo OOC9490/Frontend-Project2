642f635b56ede34b73242eb8d84949a1619ba107
doubleInc <aalavi@protonmail.com>
2020-04-07 00:44:39 +1000

    Basic flex layout for navbar-a

--- source/js/components/NavBar.js
+++ source/js/components/NavBar.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.scss';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { css } from "@emotion/core";
+import "./navbar.scss";
+
+//css values
+const header_bg_color = "grey";
+
+//css
+const Header = css`
+  /* height: 80px; */
+  background-color: ${header_bg_color};
+`;
+
+const Navbar = css`
+  padding-top: 24px;
+  display: flex;
+  /* place space between each element*/
+  justify-content: space-between;
+  /*align children properly along the cross axis of the navigation bar*/
+  align-items: center;
+`;
 
 class NavBar extends Component {
-  render(){
-      return (
-        <header>
-          <h1 className="logo">ok</h1>
-          <ul className="nav">
-            <Link to="/" className="navlink">Home</Link>
-            <li className="navlink"><a href="#">Orders</a></li>
-            <li className="navlink"><a href="#">Cart</a></li>
-            <li className="navlink"><a href="#">Search</a></li>
-          </ul>
-        </header>
-      );
+  render() {
+    return (
+      <header css={Header}>
+        <ul css={Navbar}>
+          <Link to="/" className="navlink">
+            Logo
+          </Link>
+          <Link to="/" className="navlink">
+            Home
+          </Link>
+          <li className="navlink">
+            <a href="#">Orders</a>
+          </li>
+          <li className="navlink">
+            <a href="#">Cart</a>
+          </li>
+          <li className="navlink">
+            <a href="#">Search</a>
+          </li>
+        </ul>
+      </header>
+    );
   }
-};
+}
 
 export default NavBar;
