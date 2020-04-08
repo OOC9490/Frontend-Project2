2d9444cf79c6f646eaaa068326b2da89680efe4a
doubleInc <aalavi@protonmail.com>
2020-04-09 01:26:54 +1000

    facebook approved way of class code-a

--- source/js/components/DashBoard.js
+++ source/js/components/DashBoard.js
@@ -1,35 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Card, CardTitle, CardText, CardImg, CardImgOverlay, Col, CardDeck, Container } from 'reactstrap';
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardImg,
+  CardImgOverlay,
+  Col,
+  CardDeck,
+  Container,
+} from "reactstrap";
 
 class DashBoard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stores: ["Store 1", "Store 2", "Store 3", "Store 4", "Store 5", "Store 6", "Store 7", "Store 8"],
-    };
-  }
+  state = {
+    stores: [
+      "Store 1",
+      "Store 2",
+      "Store 3",
+      "Store 4",
+      "Store 5",
+      "Store 6",
+      "Store 7",
+      "Store 8",
+    ],
+  };
 
   render() {
     return (
       <div>
-      <Container fluid>
-        <CardDeck>
-          {this.state.stores.map(s => (
-            <Col sm="3">
-              <Card inverse style={{margin:20}}>
-                <CardImg src="http://placekitten.com/100/100" alt="Card image cap" />
-                <CardImgOverlay>
-                  <CardTitle key={s}><Link to={`/dashboard/${s}`}>{s}</Link></CardTitle>
-                </CardImgOverlay>
-              </Card>
-            </Col>
-          ))}
+        <Container fluid>
+          <CardDeck>
+            {this.state.stores.map((s) => (
+              <Col sm="3">
+                <Card inverse style={{ margin: 20 }}>
+                  <CardImg
+                    src="http://placekitten.com/100/100"
+                    alt="Card image cap"
+                  />
+                  <CardImgOverlay>
+                    <CardTitle key={s}>
+                      <Link to={`/dashboard/${s}`}>{s}</Link>
+                    </CardTitle>
+                  </CardImgOverlay>
+                </Card>
+              </Col>
+            ))}
           </CardDeck>
-          </Container>
+        </Container>
       </div>
     );
   }
-};
+}
 
 export default DashBoard;
