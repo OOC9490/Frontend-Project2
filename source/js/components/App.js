c51cd819023e6d4d4e14c094094ac02439f6108f
doubleInc <aalavi@protonmail.com>
2020-04-10 02:19:15 +1000

    Hot module reloads-a

--- source/js/components/App.js
+++ source/js/components/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from "react-router-dom";
 
-import Home from './Home';
-import Navigation from './Navigation';
-import LoginUser from './LoginUser';
-import DashBoard from './DashBoard';
-import UserSignUp from './UserSignUp';
-import StorePage from './StorePage';
+import Home from "./Home";
+import Navigation from "./Navigation";
+import LoginUser from "./LoginUser";
+import DashBoard from "./DashBoard";
+import UserSignUp from "./UserSignUp";
+import StorePage from "./StorePage";
+
+import { hot } from "react-hot-loader";
 
 function App() {
-return (
-  <Router>
-    <div className="App">
-      <Navigation />
-        <Route exact path="/" component={ Home } />
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Route exact path="/" component={Home} />
         {/*<Route path="/userlogin" component={ LoginUser } />*/}
-        <Route path="/dashboard" component={ DashBoard } />
+        <Route path="/dashboard" component={DashBoard} />
         {/*<Route path="/usersignup" component={ UserSignUp } />*/}
-        <Route path="/dashboard/:storepage" component={ StorePage } />
-    </div>
-  </Router>
-);
+        <Route path="/dashboard/:storepage" component={StorePage} />
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+export default hot(module)(App);
--- source/js/components/DashBoard.js
+++ source/js/components/DashBoard.js
@@ -43,12 +43,12 @@ class DashBoard extends Component {
                     src={
                       s.image !== ""
                         ? s.image
-                        : "http://placekitten.com/100/100"
+                        : "http://placekitten.com/100/150"
                     }
                     alt="Card image cap"
                   />
                   <CardImgOverlay>
-                    <CardTitle key={s.id}>
+                    <CardTitle key={s._id}>
                       <Link to={`/dashboard/${s.name}`}>{s.name}</Link>
                     </CardTitle>
                   </CardImgOverlay>
