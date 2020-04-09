5ba885e87b73adbbadd05be0b70d99da5dc9cfab
Lawrence Carreon <l.carreon123@gmail.com>
2020-04-09 13:33:18 +1000

    applied store cart fixes

--- source/js/components/App.js
+++ source/js/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { HashRouter as Router, Route } from 'react-router-dom';
-
 import Home from './Home';
 import Navigation from './Navigation';
 import DashBoard from './DashBoard';
@@ -10,16 +8,16 @@ import UserSignUp from './UserSignUp';
 import Orders from './Orders';
 
 function App() {
-return (
-  <Router>
-    <div className="App">
-      <Navigation />
-        <Route exact path="/" component={ Home } />
-        <Route path="/dashboard/" exact component={ DashBoard } />
-        <Route path="/dashboard/:orders" component={ Orders } />
-    </div>
-  </Router>
-);
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+          <Route exact path="/" component={ Home } />
+          <Route path="/dashboard/" exact component={ DashBoard } />
+          <Route path="/dashboard/:orders" component={ Orders } />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
