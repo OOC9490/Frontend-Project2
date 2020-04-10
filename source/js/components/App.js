fcb60a9cb08bb036dc0f2452ef871562bdafba25
doubleInc <aalavi@protonmail.com>
2020-04-10 21:22:41 +1000

    merge new packages,settings into branch-a

--- source/js/components/App.js
+++ source/js/components/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from "react-router-dom";
 
-import Home from './Home';
-import Navigation from './Navigation';
-import DashBoard from './DashBoard';
-import UserSignUp from './UserSignUp';
-import Orders from './Orders';
+import Home from "./Home";
+import Navigation from "./Navigation";
+import DashBoard from "./DashBoard";
+import UserSignUp from "./UserSignUp";
+import Orders from "./Orders";
+
+// for development only
+import { hot } from "react-hot-loader";
 
 function App() {
-return (
-  <Router>
-    <div className="App">
-      <Navigation />
-        <Route exact path="/" component={ Home } />
-        <Route path="/dashboard/" exact component={ DashBoard } />
-        <Route path="/dashboard/:orders" component={ Orders } />
-    </div>
-  </Router>
-);
+  return (
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Route exact path="/" component={Home} />
+        <Route path="/dashboard/" exact component={DashBoard} />
+        <Route path="/dashboard/:orders" component={Orders} />
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+export default hot(module)(App);
